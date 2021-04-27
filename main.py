b570d4dfbe7029f8fa1cb20ae3839776955c79bd
Marinus L <marinuslentile3@gmail.com>
2021-04-27 18:08:14 +0200

    changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import math
 import numpy
-import ant as at
+
 
 class Main:
     pygame.init()
@@ -11,9 +11,7 @@ class Main:
         self.height = 700
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
-        self.foundFood = False
         self.ant_stop = False
-        self.save_coords = False
         self.fps = 30
         self.nest = Object(self.width/2-5, self.height/2-5, 10, 10, (55, 55, 55))
   
@@ -35,45 +33,104 @@ class Main:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                if event.type == pygame.KEYDOWN:
+                if event.type == pygame.KEYDOWN: 
                     if event.key == pygame.K_ESCAPE:
                         run = False 
-                if event.type == pygame.USEREVENT and not self.foundFood:
-                    for ant in ants:
-                        ant.saveCoordinates()
+
   
             # create ants one time
             while not self.ant_stop:
-                for i in range(500):
-                    ants.append(at.Ant(self.width/2, self.height/2, 1, random.randrange(3, 12)))
+                for i in range(1000):
+                    ants.append(Ant(self.width/2, self.height/2, 1, random.randrange(6, 7)))
                 self.ant_stop = True
             
             
             # draw things
             for i in range(len(ants)):
-                ants[i].update(self.screen, self.width, self.height)
+                ants[i].update(self.screen, self.width, self.height, self.nest, self.fps)
 
             food.update(self.screen)
             self.nest.update(self.screen)
 
             # food collison
             for ant in ants:
-                if not ant.back:
+                if not ant.foundFood:
                     if ant.x >= food.x and ant.x <= food.x + food.width:
                         if ant.y >= food.y and ant.y <= food.y + food.height:
-                            # save cords for one last time
-                            ant.saveCoordinates()  
-                            back_ants.append(ant)
-                            self.foundFood = True
+                            ant.foundFood = True
 
-            # move ant back to nest
-            if self.foundFood:
-                for ant in back_ants:
-                    ant.moveBack(nest)
-                    #ant.drawPath(self.screen)
-            
             pygame.display.flip()
 
+class Ant:
+    def __init__(self, x, y, radius, walk_time):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.vel = 3
+        self.rand = numpy.linspace(-self.vel, +self.vel, 100)
+        self.walk_time = walk_time
+        self.walk_count = self.walk_time
+        self.walk_list = [0, 0]
+        self.beforeCoords = []
+        self.afterCoords = []
+        self.coord_count = 0 
+        self.foundFood = False
+        self.timerRed = 1000
+        self.timerBlue = 1000
+        self.blue = 255
+        self.stepx = 0
+        self.stepy = 0
+        self.foundWay = False
+
+    def update(self, screen, width, height, nest, fps):
+        if not self.foundFood:
+            if self.walk_count == self.walk_time:
+                self.walk_list[0] = random.choice(self.rand)
+                self.walk_list[1] = random.choice(self.rand)
+                self.walk_count = 0
+        
+            self.x += self.walk_list[0]
+            self.y += self.walk_list[1]
+            self.walk_count += 1
+     
+        if self.foundFood:
+            self.moveBack(screen, nest, fps)
+
+        self.drawPath(screen)
+        # draw updated ant
+        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius)
+
+        # wall collision
+        if self.x > width:
+            self.x = width-self.radius*2
+        if self.x < 0:
+            self.x = 0+self.radius*2
+        if self.y > height:
+            self.y = height-self.radius*2
+        if self.y < 0:
+            self.y = 0+self.radius*2
+
+    def drawPath(self, screen):
+        pass
+    
+    def moveBack(self, screen, nest, fps):
+        if not self.foundWay:
+            dx, dy = (nest.x - self.x, nest.y - self.y)
+            self.stepx, self.stepy = (dx/fps, dy/fps)
+            self.foundWay = True
+
+        self.x += self.stepx
+        self.y += self.stepy
+
+        if round(self.x) == nest.x and round(self.y) == nest.y:
+            self.foundFood = False
+            
+        # pheromones
+                
+            
+
+    
+        
 
 class Object:
     def __init__(self, x, y, width, height, color):
@@ -87,7 +144,7 @@ class Object:
         pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height], border_radius=20)
         
 
-
+ 
 if __name__ == "__main__":
     main = Main()
     main.run()
